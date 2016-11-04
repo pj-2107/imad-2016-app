0d8259109b8f501b6c9d3be12beb648d84f5a1e9
pj-2107 <priyanka.jadhav@sitpune.edu.in>
2016-11-04 14:25:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var morgan = require('morgan');
+var express = require('express'); //to create a web server; for listening
+var morgan = require('morgan'); // how to respong to specific requests
 var path = require('path');
 
 var app = express();

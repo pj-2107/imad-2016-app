15d763d42a1c1e97533dc7c2e792020ad97d7469
pj-2107 <priyanka.jadhav@sitpune.edu.in>
2016-11-04 14:57:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.send("This is my Articles one page");
+    res.send("This is my Article one page");
 });
 
 app.get('/ui/style.css', function (req, res) {
